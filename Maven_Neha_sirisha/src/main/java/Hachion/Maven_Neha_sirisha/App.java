65cc3699def1139ab2b99c490cec439b94abe9a4
navya <kbnavya@gmail.com>
2023-04-05 06:33:10 +0530

    First commit

---
+++ Maven_Neha_sirisha/src/main/java/Hachion/Maven_Neha_sirisha/App.java
@@ -0,0 +1,13 @@
+package Hachion.Maven_Neha_sirisha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maven_Neha_sirisha/src/test/java/testcases/first_maven.java
@@ -0,0 +1,5 @@
+package testcases;
+
+public class first_maven {
+
+}
